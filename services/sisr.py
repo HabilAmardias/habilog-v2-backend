8e6e7f0f39063cd94c93a3f7f2c7b4fbe945ee82
Habil <muhammad.amardiaz@shopee.com>
2025-05-19 11:58:33 +0700

    add routes for SISR

--- services/sisr.py
+++ services/sisr.py
@@ -4,14 +4,14 @@ from io import BytesIO
 
 class AbstractSISRService(ABC):
     @abstractmethod
-    def upscale_image(self, upload) -> BytesIO:
+    def upscale_image(self, upload: BytesIO) -> BytesIO:
         pass
 
 class SISRServiceImpl(AbstractSISRService):
     def __init__(self, sisr_util: AbstractSISRUtil):
         self.sisr_util = sisr_util
 
-    def upscale_image(self, upload):
+    def upscale_image(self, upload: BytesIO):
         gen = self.sisr_util.load_model()
         preprocessed = self.sisr_util.preprocess_image(upload)
         out = self.sisr_util.upscale_image_with_generator(preprocessed, gen)
